e5825917d77d40dd7ff70452aee1322b163ff799
Wirenux <louis.lesniak2@gmail.com>
2026-08-19 15:58:02 +0200

    change: email

--- src/email.js
+++ src/email.js
@@ -2,7 +2,7 @@ export function initEmailLink() {
   const link = document.querySelector('[data-email-link]')
   if (!link) return
 
-  const user = 'louis'
-  const domain = 'lesniak.fr'
+  const user = 'contact'
+  const domain = 'wirenux.com'
   link.href = `mailto:${user}@${domain}`
 }
