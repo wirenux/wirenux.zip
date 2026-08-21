4d31012f2b05eb44505c9a941eaa56e492ce149b
Wirenux <louis.lesniak2@gmail.com>
2026-08-21 10:15:19 +0200

    add: vercel analytics

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './styles/main.css'
+import { inject } from "@vercel/analytics"
 import { initClock } from './clock.js'
 import { initCursor } from './cursor.js'
 import { initSmoothScroll } from './smoothScroll.js'
@@ -6,6 +7,7 @@ import { initEmailLink } from './email.js'
 import { initStack } from './stack.js'
 import { initClipboard } from './clipboard.js'
 
+inject()
 initSmoothScroll()
 initClock()
 initCursor()
